9c7577b92f4bd63072f71fc2b33c70bec9ca6548
Zarina <zor.yan4uki@gmail.com>
2023-07-28 18:31:01 +0300

    Changed HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello GoIT World!");
+        System.out.println("This is our second commit");
     }
 }
